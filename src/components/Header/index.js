3e5c82894d01e761c747ffed1e858c231fad8b1f
root <priyanshuvarma@gmail.com>
2022-04-26 05:26:45 +0000

    covid 19 dashboard

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,76 @@
-import Link from 'react-router-dom'
+import {Component} from 'react'
+import {Link} from 'react-router-dom'
+import './index.css'
 
-export default function Header() {
-  return (
-    <div className="nav-bar-large-container">
-      <Link to="/">
-        <h1>
-          COVID19<span>INDIA</span>
-        </h1>
-      </Link>
-      <ul className="nav-menu">
-        <li className="nav-menu-item">
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-        </li>
+class Header extends Component {
+  state = {isToggleActive: false}
 
-        <li className="nav-menu-item">
-          <Link to="/about" className="nav-link">
-            About
-          </Link>
-        </li>
+  whenToggleButtonClick = () => {
+    this.setState(prevState => ({isToggleActive: !prevState.isToggleActive}))
+  }
+
+  showDropDownMenu = () => (
+    <>
+      <ul className="navBar">
+        <Link to="/" className="link">
+          <li className="item">Home</li>
+        </Link>
+
+        <li className="item">Vaccination</li>
+
+        <Link to="/about" className="link">
+          <li className="item">About</li>
+        </Link>
       </ul>
-    </div>
+    </>
   )
+
+  render() {
+    const {isToggleActive} = this.state
+    console.log(isToggleActive)
+    return (
+      <>
+        <div className="header-container">
+          <Link to="/" className="link">
+            <h1 className="logo">
+              COVID19 <span className="india">INDIA </span>
+            </h1>
+          </Link>
+          <ul className="navBar">
+            <Link to="/" className="link">
+              <li className="item">Home</li>
+            </Link>
+
+            <li className="item">Vaccination</li>
+
+            <Link to="/about" className="link">
+              <li className="item">About</li>
+            </Link>
+          </ul>
+        </div>
+        <div className="mobile-menu">
+          <div className="mobile-header-container">
+            <Link to="/" className="link">
+              <h1 className="logo">
+                COVID19 <span className="india">INDIA </span>
+              </h1>
+            </Link>
+            <button
+              type="button"
+              className="toggle-button"
+              onClick={this.whenToggleButtonClick}
+            >
+              <img src="/img/add-to-queue 1.png" alt="menu" />
+            </button>
+          </div>
+
+          <div className="menu">
+            {isToggleActive ? this.showDropDownMenu() : ''}
+          </div>
+        </div>
+      </>
+    )
+  }
 }
+
+export default Header
